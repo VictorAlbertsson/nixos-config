62ccd3ba999d5528c3b0efb4b3b04e031fb13cfb
Victor Albertsson <victor.albertsson@protonmail.com>
2021-06-14 12:57:38 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+  {
+    nixosConfigurations = {
+
+      mysystem = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
+  };
+}
+
